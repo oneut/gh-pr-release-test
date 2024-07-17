9a4dad0e50dc1b2ee139812f0f28b40a1753c890
Takashi Ueno <ueno_t@openlogi.com>
2024-07-17 10:02:28 +0900

    update sync label

--- .github/workflows/scripts/sync_labels.sh
+++ .github/workflows/scripts/sync_labels.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -euo pipefail
+
 if [ $# -ne 3 ]; then
     echo "Usage: $0 <base_branch> <head_branch> <pr_number>"
     exit 1
@@ -14,10 +16,7 @@ echo "Head branch: $head_branch"
 echo "Pull Request number: $pr_number"
 
 git fetch
-commit_hashes=$(git --no-pager log origin/$base_branch..origin/$head_branch --merges --oneline --format="%H" --first-parent)
-
-# ハッシュを配列に格納
-IFS=$'\n' read -d '' -r -a hash_array <<< "$commit_hashes"
+commit_hashes=$(git --no-pager log origin/"$base_branch"..origin/"$head_branch" --merges --oneline --format="%H" --first-parent)
 
 # ユニークなラベルを格納する配列
 declare -A unique_labels
@@ -25,25 +24,29 @@ declare -A unique_labels
 # バッチ処理
 for hash in $commit_hashes; do
     echo "Processing commit: $hash"
-    echo "$search_query"
 
     # GitHub CLIを使用してプルリクエストを検索し、ラベルを含める
     # head ブランチにマージされたプルリクエストを検索
-    pr_data=$(gh pr list --state merged --base $head_branch --search "hash:$hash" --json number,title,url,mergeCommit,labels,mergedAt)
-    echo "$pr_data"
+    pr_data=$(gh pr list --state merged --base "$head_branch" --search "hash:$hash" --json number,title,url,mergeCommit,labels,mergedAt)
+    # プルリクのタイトルを表示
+    title=$(echo "$pr_data" | jq -r '.[].title')
+    echo "  PR title: $title"
 
     # jqを使用してラベルを抽出し、ユニークなラベルを追加
     labels=$(echo "$pr_data" | jq -r '.[].labels[].name')
+
+    # ラベルが見つかった場合は、
+    echo "  Labels:"
 
     if [ -n "$labels" ]; then
         while read -r label; do
             if [ -n "$label" ]; then
                 unique_labels["$label"]=1
-                echo "Found label: $label"
+                echo "    - $label"
             fi
         done <<< "$labels"
     else
-        echo "No labels found for this PR"
+        echo "    - No labels found for this PR"
     fi
 
     # APIレート制限を考慮して少し待機
@@ -61,7 +64,7 @@ done
 
 if [ -n "$labels" ]; then
     echo "Adding labels: $labels to PR #$pr_number"
-    echo gh pr edit $pr_number --add-label "$labels"
+    gh pr edit "$pr_number" --add-label "$labels"
 else
     echo "No labels to add"
 fi
